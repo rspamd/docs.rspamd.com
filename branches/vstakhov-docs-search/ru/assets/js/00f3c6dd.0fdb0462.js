7f6b67871fad92d18d359868ba28b0bb8693a0b2
vstakhov <vsevolod@rspamd.com>
2025-09-24 10:14:24 +0000

    Deploying to gh-pages from @ rspamd/docs.rspamd.com@5d025c53d354d5e12aa7efd4a830a69dd97c6213 🚀

---
+++ branches/vstakhov-docs-search/ru/assets/js/00f3c6dd.0fdb0462.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[6369],{2061:s=>{s.exports=JSON.parse('{"tags":[{"label":"performance","permalink":"/docs.rspamd.com/branches/vstakhov-docs-search/ru/blog/tags/performance","count":1},{"label":"benchmarks","permalink":"/docs.rspamd.com/branches/vstakhov-docs-search/ru/blog/tags/benchmarks","count":1}]}')}}]);
---
+++ pr/35/ru/assets/js/177bc83f.b65b49fe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[3654],{7920:s=>{s.exports=JSON.parse('{"tags":[{"label":"performance","permalink":"/docs.rspamd.com/pr/35/ru/blog/tags/performance","count":1},{"label":"benchmarks","permalink":"/docs.rspamd.com/pr/35/ru/blog/tags/benchmarks","count":1}]}')}}]);
